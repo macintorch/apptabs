23de1eb80639f5e39c00b25619fcc5015963c798
Ainor Syahrizal <ainorsyahrizal@Ainors-MacBook-Pro.local>
2016-10-31 23:53:18 +0800

    tabs displayed with swipe functionality between the tabs

---
+++ app/src/main/java/ainor/com/my/apptabs/AnnouncementFragment.java
@@ -0,0 +1,31 @@
+package ainor.com.my.apptabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ainorsyahrizal on 31/10/2016.
+ */
+
+public class AnnouncementFragment extends Fragment {
+    public AnnouncementFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_announcement, container, false);
+    }
+}
